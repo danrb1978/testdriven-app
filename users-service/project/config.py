86fb8126314416662fdb1c6a44a05cf6441ed92c
bourgeois.dan <dan.bourgeois@level3.com>
2018-02-05 15:48:35 -0700

    First functional test framework

--- users-service/project/config.py
+++ users-service/project/config.py
@@ -1,20 +1,25 @@
+import os
 
 
 class BaseConfig:
     """Base Configuration"""
     DEBUG = False
     TESTING = False
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
 
 class DevelopmentConfig(BaseConfig):
     """Development Configuration"""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     """ Testing Configuration """
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
